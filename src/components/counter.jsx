3ece3c65ebc018b40034a39fa1cee9a87ca651f9
CheeseMold <henriquebear17@gmail.com>
2022-09-25 19:06:41 -0300

    Added more state properties, dynamic tag classes, and a react list

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,22 +1,50 @@
 import React, { Component } from "react";
-import "../App.css";
 
 class Counter extends Component {
     state = {
-        count: 0,
+        count: 1,
+        imageUrl: "https://picsum.photos/200",
+        tags: ["tag1", "tag2", "tag3"],
+    };
+
+    styles = {
+        fontSize: 20,
+        fontWeight: "bold",
     };
 
     render() {
         return (
             <React.Fragment>
-                <header className="Main">
-                    <React.Fragment>
-                        <span>{this.state.count}</span>
-                    </React.Fragment>
+                <header>
+                    <img src={this.state.imageUrl} alt="not found" />
+                    <span className={this.getBadgeClass()} style={this.styles}>
+                        {this.formatCount()}
+                    </span>
                 </header>
-                <button>Increment</button>
+                <div>
+                    <button className="btn btn-secondary btn-sm">
+                        Increment
+                    </button>
+                    <ul>
+                        {this.state.tags.map((tag) => (
+                            <li key={tag}>{tag}</li>
+                        ))}
+                    </ul>
+                </div>
             </React.Fragment>
         );
+    }
+
+    formatCount() {
+        const { count } = this.state;
+        return count === 0 ? "Zero" : count;
+    }
+
+    getBadgeClass() {
+        const { count } = this.state;
+        return count === 0
+            ? "badge badge-warning m-5"
+            : "badge badge-primary m-5";
     }
 }
 
